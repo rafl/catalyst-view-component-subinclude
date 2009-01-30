53829a5d291cacbadebf0b94469ccfeff37c072d
Nilson Santos Figueiredo Junior <nilsonsfj@cpan.org>
2009-01-30 19:34:34 +0000

    Add docs to main module

--- lib/Catalyst/View/Component/SubInclude.pm
+++ lib/Catalyst/View/Component/SubInclude.pm
@@ -4,6 +4,75 @@ use Moose::Role;
 use Carp qw/croak/;
 use namespace::clean qw/croak/;
 
+=head1 NAME
+
+Catalyst::View::Component::SubInclude - Use subincludes in your Catalyst views
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+  package MyApp::View::TT;
+  use Moose;
+
+  extends 'Catalyst::View::TT';
+  with 'Catalyst::View::Component::SubInclude';
+
+  __PACKAGE__->config( subinclude_plugin => 'SubRequest' );
+
+Then, somewhere in your templates:
+
+  [% subinclude('/my/widget') %]
+
+=head1 DESCRIPTION
+
+C<Catalyst::View::Component::SubInclude> allows you to include content in your
+templates (or, more generally, somewhere in your view's C<render> processing)
+which comes from another action in your application. It's implemented as a 
+L<Moose::Role>, so using L<Moose> in your view is required.
+
+Simply put, it's a way to include the output of a Catalyst sub-request somewhere
+in your page. 
+
+It's built in an extensible way so that you're free to use sub-requests, Varnish 
+ESI (L<http://www.catalystframework.org/calendar/2008/17>) or any other 
+sub-include plugin you might want to implement. An LWP plugin seems useful and
+might be developed in the future.
+
+=head1 STASH FUNCTION
+
+This component does its magic by exporting a C<subinclude> coderef entry to the
+stash. This way, it's easily accessible by the templates (which is the most 
+common use-case).
+
+=head2 C<subinclude( $path, @args )>
+
+This will return the body of the requested resource (as specified by C<$path>).
+
+=head1 SUBINCLUDE PLUGINS
+
+The module comes with two subinclude plugins: 
+L<SubRequest|Catalyst::Plugin::View::Component::SubRequest> and 
+L<ESI|Catalyst::Plugin::View::Component::ESI>.
+
+By default, the SubRequest plugin will be used. This can be changed in the 
+view's configuration options (either in the config file or in the view module
+itself). 
+
+Configuration file example:
+
+  <View::TT>
+      subinclude_plugin   ESI
+  </View::TT>
+
+=cut
+
 has 'subinclude_plugin' => (
     is => 'rw',
     isa => 'ClassName'
@@ -36,5 +105,37 @@ around 'render' => sub {
 
     $self->$next( $c, @args );
 };
+
+=head1 SEE ALSO
+
+L<Catalyst::Plugin::SubRequest>, L<Moose::Role>, L<Moose>,
+L<http://www.catalystframework.org/calendar/2008/17>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-catalyst-view-component-subinclude at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Catalyst-View-Component-SubInclude>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 AUTHOR
+
+Nilson Santos Figueiredo Junior, C<< <nilsonsfj at cpan.org> >>
+
+=head1 SPONSORSHIP
+
+Development sponsored by Ionzero LLC L<http://www.ionzero.com/>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (C) 2009 Nilson Santos Figueiredo Junior.
+
+Copyright (C) 2009 Ionzero LLC.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
 
 1;
