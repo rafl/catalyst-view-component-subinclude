b4e18e618c8ade72e4992e24513786f19893f11a
Nilson Santos Figueiredo Junior <nilsonsfj@cpan.org>
2009-01-30 18:23:15 +0000

    Rename to Catalyst-View-Component-SubInclude

--- lib/Catalyst/View/Component/SubInclude/SubRequest.pm
+++ lib/Catalyst/View/Component/SubInclude/SubRequest.pm
@@ -5,6 +5,10 @@ use strict;
 sub generate_subinclude {
     my ($class, $c, $path, $params) = @_;
     my $stash = {};
+
+    croak "subincludes through subrequests require Catalyst::Plugin::SubRequest"
+        unless $c->can('sub_request');
+
     $c->sub_request( $path, $stash, $params );
 }
 
