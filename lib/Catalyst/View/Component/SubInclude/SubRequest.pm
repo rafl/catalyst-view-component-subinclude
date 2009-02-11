72b6f9796f5f09de2ec3723ec1b1e53bf0688fcb
Nilson Santos Figueiredo Junior <nilsonsfj@cpan.org>
2009-02-11 02:36:52 +0000

    Bump to 0.04

--- lib/Catalyst/View/Component/SubInclude/SubRequest.pm
+++ lib/Catalyst/View/Component/SubInclude/SubRequest.pm
@@ -11,11 +11,11 @@ Catalyst::View::Component::SubInclude::SubRequest - Sub-requests plugin for C::V
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
--- lib/Catalyst/View/Component/SubInclude/Visit.pm
+++ lib/Catalyst/View/Component/SubInclude/Visit.pm
@@ -11,11 +11,11 @@ Catalyst::View::Component::SubInclude::Visit - visit() plugin for C::V::Componen
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
